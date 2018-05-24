b92975192d2afbcbaace32e01e5e9d6587a88a8f
Mary-Kate James <marykatj@gmail.com>
2018-05-24 16:10:30 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,7 @@ end
 
 def checkout(cart, coupons)
   cart = consolidate_cart(cart:[])
-  cart1 = apply_coupons(cart, coupons:[])
+  cart1 = apply_coupons(cart:[], coupons:[])
   cart2 = apply_clearance(cart1)
 
   total = 0

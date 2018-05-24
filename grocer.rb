05269c7dd42a0227f4037355e772e38864bd885c
Mary-Kate James <marykatj@gmail.com>
2018-05-24 15:24:51 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -6,8 +6,8 @@ def consolidate_cart(cart)
     food_hash.each do |name, price_hash|      # for each food_hash, name and price_hash
       if new_hash[name].nil?                  # if our new hash does not have a name already...
         new_hash[name] = price_hash.merge({:count => 1})    # ... add name to new_hash and merge price and count 1.
-      else
-        new_hash[name][:count] += 1
+      else                                    # otherwise, if new_hash does have that specific name...
+        new_hash[name][:count] += 1           # ..increase the counter by 1
       end
     end
   end
